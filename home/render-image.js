5e3ac5955e5f52f54e8521ee22058e0e76181da7
sethripman <sethripman@gmail.com>
2019-10-29 11:20:43 -0700

    TDDed a render function

--- home/render-image.js
+++ home/render-image.js
@@ -1,13 +1,13 @@
-export default function renderImage() {
+export default function renderImage(item) {
     const html = /*html*/`
     <li>
         <div class="info-container">
-            <h2>Uniwhal</h2>
-            <p class="horns">1 Horn</p>
+            <h2>${item.title}</h2>
+            <p class="horns">${item.horns} Horn</p>
         </div>
 
         <div class="image-container">
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" title="A unicorn and a narwhal nuzzling their horns">
+            <img src="${item.url}" title="${item.description}">
         </div>
     </li>
     `;
--- test/render-test.js
+++ test/render-test.js
@@ -7,10 +7,18 @@ const test = QUnit.test;
 test('should render a list item', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const input = {
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        title: 'UniWhal',
+        description: 'A unicorn and a narwhal nuzzling their horns',
+        keyword: 'narwhal',
+        horns: 1
+    };
+
     const expected = /*html*/`
     <li>
         <div class="info-container">
-            <h2>Uniwhal</h2>
+            <h2>UniWhal</h2>
             <p class="horns">1 Horn</p>
         </div>
 
@@ -21,9 +29,9 @@ test('should render a list item', function(assert) {
     `;
     //Act 
     // Call the function you're testing and set the result to a const
-    const output = renderImage();
+    const output = renderImage(input);
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.htmlEqual(expected, output);
+    assert.htmlEqual(output, expected);
 });
